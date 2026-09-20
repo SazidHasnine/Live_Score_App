ce5c4098f9b6b5827542ec84057ee4fc81c906a1
Your Name <sazidhas9steam@gmail.com>
2026-09-20 09:39:15 +0600

    Core feature working

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -48,6 +45,16 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyC4VydFtg9Og-N2jOWCRIqV1_U7XPY6fqc',
+    appId: '1:518249660125:web:62e60ed8cb46281d6a2534',
+    messagingSenderId: '518249660125',
+    projectId: 'live-score-app-4b7bc',
+    authDomain: 'live-score-app-4b7bc.firebaseapp.com',
+    storageBucket: 'live-score-app-4b7bc.firebasestorage.app',
+    measurementId: 'G-CNDENY78ML',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyD4yiVzLwhiaCYSag3ZEfkxueiwbEDwaVk',
